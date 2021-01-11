5d42be3d5dfbfab16683a48bfdf4eb738f0fc17f
greg mathias <mathiasgreg@gmail.com>
2021-01-11 18:44:56 -0500

    Change folder structure

--- app.js
+++ app.js
@@ -1,79 +1,292 @@
-// computerPlay function - randomly return computers play
-var playerSelection = prompt("Enter rock, paper or scissors");
-var computerSelection = computerPlay();
+window.onload = beginningAnimation();
+let computerSelection;
+let playerSelection;
+let computerScore = 0;
+let playerScore = 0;
+
+let buttons = document.querySelectorAll(".button");
+const body = document.querySelector("body");
+const main = document.querySelector("main");
+const endAlrt = document.querySelector("#end-alert");
+const endDesc = document.querySelector("#end-desc");
+const returnMainBtn = document.querySelector("#retry-btn");
+const desc = document.querySelector("#desc3");
+const container = document.querySelector("#results-container");
+
+body.addEventListener("click", skipAnime());
+body.addEventListener("keydown", skipAnime());
+
+function skipAnime() {
+    const span = document.querySelectorAll("span");
+
+    span.forEach((span) => span.classList.add("skip"));
+}
+
+function beginningAnimation() {
+    fadeIn();
+    // turn nodelist of spans to array to access last value for onTransitionEnd
+    const desc1 = document.querySelector("#desc1");
+    let desc1Span = desc1.querySelectorAll("span");
+
+    desc1Span = Array.from(desc1Span);
+
+    const desc2 = document.querySelector("#desc2");
+    const desc3 = document.querySelector("#desc3");
+
+    desc1Span[desc1Span.length - 1].ontransitionend = () => {
+        desc1.classList.add("fade-out");
+
+        desc1.addEventListener("animationend", () => {
+            desc1.classList.add("disappear");
+            desc1.classList.remove("animate");
+            desc2.classList.remove("disappear");
+            desc2.classList.add("animate");
+            fadeIn();
+            // collect nodelist of span in same function of fadeIn() or 
+            // nodelist will be empty
+
+            let desc2Span = desc2.querySelectorAll("span")
+            desc2Span = Array.from(desc2Span);
+
+            desc2Span[desc2Span.length - 1].ontransitionend = () => {
+                desc2.classList.add("fade-out");
+                desc2.addEventListener("animationend", () => {
+                    desc2.classList.add("disappear");
+                    desc2.classList.remove("animate");
+                    desc3.classList.remove("disappear");
+                    desc3.classList.add("animate");
+                    fadeIn();
+
+                    let desc3Span = desc3.querySelectorAll("span");
+                    desc3Span = Array.from(desc3Span);
+
+                    desc3Span[desc3Span.length -1].ontransitionend = () => {
+                        const cta = document.querySelector("#cta");
+
+                        cta.classList.add("drop-down");
+
+                        cta.addEventListener("animationend", () => {
+                            const gameCtn = document.querySelector("#game-container");
+
+                            setTimeout(function () {
+                                gameCtn.classList.add("fade-in");
+                            }, 300);
+                        })
+                    }
+                })
+            }
+        })
+    }
+}
+function fadeIn() {
+    let text = document.querySelector(".animate");
+
+    let strText = text.textContent;
+    let splitText = strText.split("");
+    text.textContent = "";
+    // append span tags to each character in the string
+    for (i = 0; i < splitText.length; i++) {
+        text.innerHTML += `<span>${splitText[i]}</span>`;
+    }
+
+    let char = 0;
+    let timer = setInterval(onTick, 50);
+
+    function onTick() {
+        const span = text.querySelectorAll("span")[char];
+        span.classList.add("fade");
+        char++;
+        // stop function at end of string
+        if (char === splitText.length) {
+            complete();
+            return;
+        }
+    }
+    function complete() {
+        clearInterval(timer);
+        timer = null;
+    }
+}
+
+buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+        const img = button.querySelector("img");
+        playerSelection = img.alt.toLowerCase();
+
+        playRound(playerSelection, computerSelection);
+
+        if (playerScore === 5 || computerScore === 5) {
+            declareWinner();
+        }
+    })
+})
+
+const myArray = ["Rock", "Paper", "Scissors"];
 
 function computerPlay() {
-    let rps = ["rock", "paper", "scissors"];
-    const pick = Math.floor(Math.random() * rps.length);
-    return rps[pick] 
+    return myArray[~~(Math.random() * myArray.length)];
 }
-
-
-let yourScore = 0;
-let computerScore = 0;
-let results = [];
-
-
-// playerSelection - get user play 
-
-// single round - compare computerSelection and playerSelection and return which is winner string and counter++ winner
 
 function playRound(playerSelection, computerSelection) {
-    if (
-        (playerSelection == "rock" || playerSelection == "Rock" || playerSelection == "ROCK") &&
-        (computerSelection == "scissors" || computerSelection == "Scissors" || computerSelection == "SCISSORS") 
-        ||
-        (playerSelection == "paper" || playerSelection == "Paper" || playerSelection == "PAPER") && 
-        (computerSelection == "rock" || computerSelection == "Rock" || computerSelection == "ROCK") &&
-        (yourScore <= 5 || computerScore <= 5)) {
-            
-            results.push(`You win: ${playerSelection} beats ${computerSelection}!`);
-            yourScore++;
-            if (yourScore >= 5) {
-                results.push(`You have 5 points - you win!`)
-            }
-            
-        } else if (
-            (playerSelection == "rock" || playerSelection == "Rock" || playerSelection == "ROCK") &&
-            (computerSelection == "paper" || computerSelection == "Paper" || computerSelection == "PAPER") 
-            || 
-            (playerSelection == "paper" || playerSelection == "Paper" || playerSelection == "PAPER") &&
-            (computerSelection = "scissors" || computerSelection == "Scissors" || computerSelection == "SCISSORS") &&
-            (yourScore <= 5 || computerScore <= 5)) {
-                results.push(`Computer wins: ${computerSelection} beats ${playerSelection}`);
-                computerScore++;
-                if (computerScore >= 5) {
-                    results.push(`Computer has 5 points - it wins!`);
-                }
-            } else {
-              //  results.push("You tied")
-            }
-        }        
-       
-
-function game() {
-    while (yourScore < 5 && computerScore < 5) {
-        playRound(prompt("Enter rock, paper or scissors"), computerPlay());
-    
-       // playerSelection = playerSelection.toLowerCase();
-    
-// //    if (yourScore > computerScore) {
-//         console.log("You win");
-//     } else if (computerScore > yourScore) {
-//         console.log("Computer wins");
-//     } else {
-//         console.log('You tied');
-//     }
+    computerSelection = computerPlay().toLowerCase();
+    playerSelection = playerSelection.toLowerCase();
+    if (computerSelection == playerSelection) {
+        displayResults("It's a tie!")
+    } else if (
+      (computerSelection == "rock" && playerSelection == "scissors") ||
+      (computerSelection == "scissors" && playerSelection == "paper") ||
+      (computerSelection == "paper" && playerSelection == "rock")
+    ) {
+      computerScore = ++computerScore;
+      keepCpuScore();
+      if (computerScore === 1) {
+        displayResults(
+                `You lost.
+                ${capitalize(computerSelection)} beats ${playerSelection}.`
+            );
+      } else if (computerScore === 2) {
+            displayResults(
+                `Machine lucky. ${capitalize(computerSelection)} beats ${playerSelection}.`
+            );
+      } else if (computerScore === 3) {
+            displayResults(
+                `It's just electrons arranged in silicon, don't give up. ${capitalize(computerSelection)} beats ${playerSelection}.`
+            );
+      } else if (computerScore === 4) {
+            displayResults(
+                `Match point! ${capitalize(computerSelection)} beats
+                ${playerSelection}.`
+            );
+      } else {
+          displayResults(`${computerSelection} beats ${playerSelection}`);
+      }
+    } else {
+      playerScore = ++playerScore;
+      keepPlayerScore();
+      if (playerScore === 1) {
+        displayResults(
+           `Easy peezy lemon squeezy. ${capitalize(playerSelection)} beats ${computerSelection}.`
+        );
+       } else if (playerScore === 2) {
+         displayResults(
+            `Man > Machine. ${capitalize(
+               playerSelection
+            )} beats ${computerSelection}.`
+         );
+       } else if (playerScore === 3) {
+         displayResults(
+            `${capitalize(
+             playerSelection
+            )} beats ${computerSelection}. In before the Singularity?`
+         );
+       } else if (playerScore === 4) {
+         displayResults(
+           `${capitalize(
+            playerSelection
+           )} beats ${computerSelection}. Ad In!`
+         );
+       } else {
+         displayResults(`${playerSelection} beats ${computerSelection}`);
+       }
+    }
 }
+
+
+
+
+
+
+
+function capitalize(str) {
+  return str.charAt(0).toUpperCase() + str.slice(1);
 }
-game();
+
+function displayResults(str) {
+  container.animate([{ opacity: 0}, { opacity: 1 }], {
+        duration: 300,
+        fill: "forwards",
+        iterations: 1,
+        delay: 0,
+        easing: "ease-out",
+  });
+  container.textContent = str;
+}
+
+function declareWinner() {
+  rplContent();
+  if (playerScore > computerScore) {
+    endDesc.textContent = "You win!(sim#44kkslfff::let.human.win)";
+    returnMainBtn.innerText = "Play again";
+  } else {
+    endDesc.textContent = "You lost.";
+    returnMainBtn.innerText = "Play again";
+  }
+  fadeIn();
+
+  let endDescSpan = endDesc.querySelectorAll("span");
+  endDescSpan = Array.from(endDescSpan);
+
+  endDescSpan[endDescSpan.length - 1].ontransitionend = () => {
+    returnMainBtn.classList.add("fade-in");
+    }
+}
 
 
-const title = document.getElementById('title');
-title.textContent = "Rock, Paper, Scissors";
 
-const score = document.createElement('div');
-title.appendChild(score);
-score.textContent = results
+
+
+
+
+
+function rplContent() {
+  main.classList.add("disappear");
+  endAlrt.classList.remove("disappear")
+  desc.classList.remove("animate");
+  endDesc.classList.add("animate");
+    
+  returnMainBtn.addEventListener("click", () => {
+    main.classList.remove("diappear");
+    endAlrt.classList.add("disappear");
+    desc.classList.add("animate");
+    returnMainBtn.classList.remove("fade-in");
+    resetGame();
+  });
+}
+
+function resetGame() {
+  fadeIn();
+  container.textContent = "";
+  playerScore = 0;
+  computerScore = 0;
+  keepPlayerScore();
+  keepCpuScore();
+}
+
+function keepPlayerScore() {
+  let playerScoreBox = document.querySelector("#player-score");
+
+  playerScoreBox.animate([{ opacity: 0}, { opacity: 1 }], {
+    duration: 300,
+    fill: "forwards",
+    iterations: 1,
+    delay: 0,
+    easing: "ease-out",
+  });
+
+  playerScoreBox.textContent = playerScore;
+}
+function keepCpuScore() {
+  let computerScoreBox = document.querySelector("#computer-score");
+
+  computerScoreBox.animate([{ opacity: 0 }, { opacity: 1 }], {
+    duration: 300,
+    fill: "forwards",
+    iterations: 1,
+    delay: 0,
+    easing: "ease-out",
+  })
+
+  computerScoreBox.textContent = computerScore;
+}
 
 
